0c2b886c637ba515094cdbcd84164146f0aa23de
Makutu369 <brightdreamfuture6@gmail.com>
2024-06-04 15:06:11 +0000

    added database access

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 const PORT = process.env.PORT || 6000;
 mongoose
-  .connect("mongodb://localhost/server")
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("db success"))
   .catch((err) => console.log(err.message));
 
